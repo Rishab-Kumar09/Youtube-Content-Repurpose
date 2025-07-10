97d82dd2c54411e446196355dddef3a2f190c602
Your Name <user@example.com>
2025-07-10 12:45:53 -0500

    Update CORS headers to allow GET requests

--- netlify/functions/webhook-proxy.js
+++ netlify/functions/webhook-proxy.js
@@ -5,7 +5,7 @@ exports.handler = async (event) => {
   const headers = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers': 'Content-Type',
-    'Access-Control-Allow-Methods': 'POST, OPTIONS',
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
   };
 
   // Handle preflight
